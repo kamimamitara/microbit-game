88065ba0fd43feac2806d96b45e1c9a5d80447c3
kamimamitara <77587068+kamimamitara@users.noreply.github.com>
2021-06-18 10:03:11 -0400

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -65,23 +65,42 @@ basic.pause(1000)
 basic.clear_screen()
 
 def on_received_value(name, value):
-    ship = [int(name),value]
-    if (ship == ship1):
-        ship1 = [-1,-1]
-    if (ship == ship2):
-        ship1 = [-1,-1]
-    if (ship == ship3):
-        ship1 = [-1,-1]
+    global ship1
+    global ship2
+    global ship3
+    if name == "hit":
+        basic.show_string("Hit!")
+    else:
+        ship = [int(name),value]
+        if (ship[0] == ship1[0] and ship[1] == ship1[1]):
+            radio.send_value("hit", 1)
+            ship1 = [-1,-1]
+        if (ship[0] == ship2[0] and ship[1] == ship2[1]):
+            radio.send_value("hit", 2)
+            ship2 = [-1,-1]
+        if (ship[0] == ship3[0] and ship[1] == ship3[1]):
+            radio.send_value("hit", 3)
+            ship3 = [-1,-1]
 radio.on_received_value(on_received_value)
+
+
 
 while True:
     if (ship1== [-1,-1] and ship2 == [-1,-1] and ship3== [-1,-1]):
-        break
+        basic.clear_screen()
+        basic.show_string("You lose!")
     input.on_button_pressed(Button.A, on_button_pressed_a)
     input.on_button_pressed(Button.B, on_button_pressed_b)
     led.toggle(a,b)
+    led.toggle(a,b)
     basic.pause(750)
     led.toggle(a,b)
-    radio.send_value(str(a), b)
-basic.clear_screen()
-basic.show_string("You lose!")
+    if input.logo_is_pressed():
+        basic.clear_screen()
+        basic.show_string("c")
+        basic.pause(1000)
+        basic.clear_screen()
+        radio.send_value(str(a), b)
+        a = 0
+        b = 0
+
--- main.ts
+++ main.ts
@@ -61,32 +61,52 @@ basic.showIcon(IconNames.Yes)
 basic.pause(1000)
 basic.clearScreen()
 radio.onReceivedValue(function on_received_value(name: string, value: number) {
-    let ship1: number[];
-    let ship = [parseInt(name), value]
-    if (ship == ship1) {
-        ship1 = [-1, -1]
-    }
+    let ship: number[];
     
-    if (ship == ship2) {
-        ship1 = [-1, -1]
-    }
     
-    if (ship == ship3) {
-        ship1 = [-1, -1]
+    
+    if (name == "hit") {
+        basic.showString("Hit!")
+    } else {
+        ship = [parseInt(name), value]
+        if (ship[0] == ship1[0] && ship[1] == ship1[1]) {
+            radio.sendValue("hit", 1)
+            ship1 = [-1, -1]
+        }
+        
+        if (ship[0] == ship2[0] && ship[1] == ship2[1]) {
+            radio.sendValue("hit", 2)
+            ship2 = [-1, -1]
+        }
+        
+        if (ship[0] == ship3[0] && ship[1] == ship3[1]) {
+            radio.sendValue("hit", 3)
+            ship3 = [-1, -1]
+        }
+        
     }
     
 })
 while (true) {
     if (ship1 == [-1, -1] && ship2 == [-1, -1] && ship3 == [-1, -1]) {
-        break
+        basic.clearScreen()
+        basic.showString("You lose!")
     }
     
     input.onButtonPressed(Button.A, on_button_pressed_a)
     input.onButtonPressed(Button.B, on_button_pressed_b)
     led.toggle(a, b)
+    led.toggle(a, b)
     basic.pause(750)
     led.toggle(a, b)
-    radio.sendValue("" + a, b)
+    if (input.logoIsPressed()) {
+        basic.clearScreen()
+        basic.showString("c")
+        basic.pause(1000)
+        basic.clearScreen()
+        radio.sendValue("" + a, b)
+        a = 0
+        b = 0
+    }
+    
 }
-basic.clearScreen()
-basic.showString("You lose!")
